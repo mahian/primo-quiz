8f0ffef879d42be4916585bd4e183f031fc8cb77
mahian ahmad <mahianahamd9@gmail.com>
2022-10-11 21:28:33 +0600

    create blog section map

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 const Blog = () => {
+    const blogs = [
+        {id: 1, title: 'title', body: 'body'},
+        {id: 1, title: 'title', body: 'body'},
+        {id: 1, title: 'title', body: 'body'},
+    ]
     return (
         <section className='p-10'>
             <h2 className='text-3xl font-semibold mb-10 text-center'>Blog</h2>
-
-            <h2 className='text-5xl font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-            <p className='mt-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus hic, repellat odio omnis quasi maxime quo voluptate consectetur! Distinctio libero at necessitatibus quidem, similique, minima deserunt repudiandae neque perferendis temporibus nihil velit quaerat vel adipisci nemo!</p>
+            {
+                blogs.map(blog => <div key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <p>{blog.body}</p>
+                </div>)
+            }
         </section>
     );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+import { ArrowRightIcon, BookOpenIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -5,11 +6,11 @@ const Hero = () => {
     return (
         <section className='py-10'>
             <div className='flex flex-col items-center text-center p-10'>
-                <h1 className='text-6xl font-semibold'>Quize Website Title</h1>
-                <p className='text-gray-600 mt-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur beatae tempore quod animi illo, <br /> corporis quia ex, dolor quos amet nemo, velit voluptates!</p>
-                <div className='mt-4'>
-                    <Link to='blog'><button className='btn prime-btn mr-5'>Learn More</button></Link>
-                    <Link to='quizes'><button className='btn second-btn'>Get Quize</button></Link>
+                <h1 className='text-6xl font-bold'>Improve your knowledge</h1>
+                <p className='text-gray-600 mt-5'>Read our blogs to learn new things and participate in quizzes with us to <br /> improve your knowledge.</p>
+                <div className='mt-5 flex'>
+                    <Link to='blog'><button className='btn prime-btn mr-5 flex items-center'>Learn More <BookOpenIcon className="w-6 h-6 ml-3"/></button></Link>
+                    <Link to='quizes'><button className='btn second-btn flex items-center'>Get Quize <ArrowRightIcon className="w-6 h-6 ml-3"/></button></Link>
                 </div>
             </div>
             <div className='p-10 flex justify-center items-center'>
